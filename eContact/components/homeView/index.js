48c57d21570243f116c98f1286fe29dc6fd07c51
André Tong <andretong@gmail.com>
2016-06-17 18:46:58 +0000

    Changes made by André Tong (andretong@gmail.com).

--- eContact/components/homeView/index.js
+++ eContact/components/homeView/index.js
@@ -8,15 +8,13 @@ app.homeView = kendo.observable({
         var tree = JSON.parse(sessionStorage.getItem("tree"));
         var treeID = tree.getPostTreeInfoResult.TreeID;
         var nodoID = tree.getPostTreeInfoResult.StartingNodeID;
-        app.consultarNode(treeID, nodoID);        
-        app.consultarNodeActions(nodoID); 
         
-        var node =  JSON.parse(sessionStorage.getItem("node_"+nodoID));
+        var node = JSON.parse(sessionStorage.getItem("node_" + nodoID));
         var menu = node.getPostNodeResult;
-        
-        var nodeActions = JSON.parse(sessionStorage.getItem("nodeActions"));        
+
+        var nodeActions = JSON.parse(sessionStorage.getItem("nodeActions"));
         var actions = nodeActions.getPostNodeActionsResult;
-        
+
         /*var menu = tree.startingNode.nodeMenuAttributes.options;
         var optionsMenu = new kendo.data.DataSource({
             data: menu
@@ -30,21 +28,19 @@ app.homeView = kendo.observable({
             data: menu
         });
         app.homeView.set('menu', optionsMenu);
-        
+
         var dataSourceActions = new kendo.data.DataSource({
             data: actions
         });
         app.homeView.set('dataActionsHome', dataSourceActions);
 
-        
         $("#images").kendoMobileListView({
             dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
             template: "<img src='#: data #'>"
         });
-        
-    },
-    afterShow: function () {
 
+    },
+    afterShow: function () {        
     },
     logout: function () {
         app.logout();
@@ -64,12 +60,17 @@ app.homeView = kendo.observable({
         app.clienteDS.read();
     }
 
-    if (sessionStorage.getItem("tree") == null || sessionStorage.getItem("tree") == '') {
-        var datosIVR = JSON.parse(sessionStorage.getItem("datosIVR"));
-        app.consultarTreeIVR(datosIVR.getPosAuthenticateResult.TreeID);
-        //app.treeDS.read();                       
-    }
+    //if (sessionStorage.getItem("tree") == null || sessionStorage.getItem("tree") == '') {
+    var datosIVR = JSON.parse(sessionStorage.getItem("datosIVR"));
+    app.consultarTreeIVR(datosIVR.getPosAuthenticateResult.TreeID);
+    //app.treeDS.read();                       
+    //}
 
+    var tree = JSON.parse(sessionStorage.getItem("tree"));
+    var treeID = tree.getPostTreeInfoResult.TreeID;
+    var nodoID = tree.getPostTreeInfoResult.StartingNodeID;
+    app.consultarNode(treeID, nodoID);
+    app.consultarNodeActions(nodoID);
 
     //app.consultarNodeActions(10);
     //app.nodeActionsDS.read();
