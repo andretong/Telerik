86a73b834d2ee3c048b549c3b3f68151be4e4c31
André Tong <andretong@gmail.com>
2016-06-29 22:25:33 +0000

    Changes made by André Tong (andretong@gmail.com).

--- eContact/components/infoView/index.js
+++ eContact/components/infoView/index.js
@@ -3,15 +3,17 @@
 
 
 app.infoView = kendo.observable({
-    onShow: function () {
-    },
-    afterShow: function () {}
+    onShow: function () {},
+    afterShow: function () {},
+    register: function () {
+        alert("Guardando");
+    }
 });
 
 // START_CUSTOM_CODE_settingsView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 (function () {
-    app.infoView.set('title', 'Información');
+    app.infoView.set('title', 'Ingrese sus Datos');
 
 
 })();
